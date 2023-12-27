8e10b52b65c8d6e933ec5f665fd19a034a0792bd
DCShaw-212 <79878995+DCShaw-212@users.noreply.github.com>
2023-12-27 09:54:39 -0500

    Add files via upload

---
+++ CSHARP/Shaw_Assign_3/Shaw_Assn_3/Assn3Template/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Assn3Template
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOneIteration = new System.Windows.Forms.Button();
+            this.iterationCount = new System.Windows.Forms.NumericUpDown();
+            this.multiIteration = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.iterationCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOneIteration
+            // 
+            this.btnOneIteration.Location = new System.Drawing.Point(12, 464);
+            this.btnOneIteration.Name = "btnOneIteration";
+            this.btnOneIteration.Size = new System.Drawing.Size(297, 44);
+            this.btnOneIteration.TabIndex = 0;
+            this.btnOneIteration.Text = "Run One Iteration";
+            this.btnOneIteration.UseVisualStyleBackColor = true;
+            this.btnOneIteration.Click += new System.EventHandler(this.btnOneIteration_Click);
+            // 
+            // iterationCount
+            // 
+            this.iterationCount.Location = new System.Drawing.Point(357, 476);
+            this.iterationCount.Name = "iterationCount";
+            this.iterationCount.Size = new System.Drawing.Size(104, 22);
+            this.iterationCount.TabIndex = 2;
+            // 
+            // multiIteration
+            // 
+            this.multiIteration.Location = new System.Drawing.Point(522, 464);
+            this.multiIteration.Name = "multiIteration";
+            this.multiIteration.Size = new System.Drawing.Size(316, 44);
+            this.multiIteration.TabIndex = 4;
+            this.multiIteration.Text = "Run Multiple Iteration";
+            this.multiIteration.UseVisualStyleBackColor = true;
+            this.multiIteration.Click += new System.EventHandler(this.multiIteration_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 554);
+            this.Controls.Add(this.multiIteration);
+            this.Controls.Add(this.iterationCount);
+            this.Controls.Add(this.btnOneIteration);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.iterationCount)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOneIteration;
+        private System.Windows.Forms.NumericUpDown iterationCount;
+        private System.Windows.Forms.Button multiIteration;
+    }
+}
+
---
+++ CSHARP/Shaw_Assign_3/Shaw_Assn_3/Assn3Template/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Assn3Template
+{
+    public partial class Form1 : Form
+    {
+        const int PANEL_SIZE = 25;
+        const int SQUARE_SIZE = 10;
+        Panel[,] panelArray = new Panel[SQUARE_SIZE, SQUARE_SIZE];
+        Color[,] colorArray = new Color[SQUARE_SIZE, SQUARE_SIZE];
+        
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < SQUARE_SIZE; i++)
+            {
+                for (int j = 0; j < SQUARE_SIZE; j++)
+                {
+                    Panel nextPanel = new Panel();
+                    panelArray[i, j] = nextPanel;
+                    nextPanel.BackColor = Color.DarkGray;
+                    nextPanel.Size = new Size(PANEL_SIZE, PANEL_SIZE);
+                    nextPanel.Location = new Point(10 + (PANEL_SIZE + 10) * j, 10 + (PANEL_SIZE + 10) * i);
+                    nextPanel.Click += new EventHandler(clickPanel);
+
+                    ((Form)sender).Controls.Add(nextPanel);
+
+
+                }
+
+            }
+        }
+
+        private void clickPanel(object sender, EventArgs e)
+        {
+            //Initialize the variable for the panel
+
+            Panel colorPanel = (Panel)(sender);
+
+            //Conditional statements to change panel color when clicked.
+
+            if (colorPanel.BackColor == Color.DarkGray)
+            {
+                colorPanel.BackColor = Color.Green;
+            }
+            else if (colorPanel.BackColor == Color.Green)
+            {
+                colorPanel.BackColor = Color.DarkGray;
+            }
+
+
+
+        }
+
+        private void btnOneIteration_Click(object sender, EventArgs e)
+        {
+            RunOneIteration();
+        }
+
+        //Main part of the program
+
+
+
+        //Function to test if indicies are valid
+
+        public static bool IndicieTest(int value1, int value2)
+        {
+
+            if (value1 < 0 && value2 < 0)
+                return false;
+            else if (value1 >= SQUARE_SIZE && value2 >= SQUARE_SIZE)
+                return false;
+            else
+                return true;
+        }
+
+        // Runs the game and sets the colors
+
+        public void RunOneIteration()
+        {
+            for (int i = 0; i < SQUARE_SIZE; i++)
+            {
+                for (int j = 0; j < SQUARE_SIZE; j++)
+                {
+
+                    
+                    Color[,] huePanel = new Color[SQUARE_SIZE, SQUARE_SIZE];
+
+
+                    foreach (Panel panel in panelArray)
+
+                    {
+                        int neighbors = 0;
+                        //Checks indicie validation while it's true run
+
+                        
+
+                        //Checks if the color is green, if yes add to neighbors
+
+                        //Checks neighbor to the east
+                       if (IndicieTest(i, j + 1))
+                       {
+                            if (huePanel[i, j + 1] == Color.Green)
+                              neighbors++;
+                       }
+
+                        //Checks neighbor to the southeast
+                       if (IndicieTest(i + 1, j + 1))
+                        {
+                            if (huePanel[i + 1, j + 1] == Color.Green)
+                                neighbors++;
+                        }
+
+                        //checks neighbor to the south
+                        if (IndicieTest(i + 1, j) && huePanel[i + 1, j] == Color.Green)
+                        {
+                            if (huePanel[i + 1, j] == Color.Green)
+                                neighbors++;
+                        }
+
+                        //checks neighbor to the the southwes
+                        if (IndicieTest(i + 1, j - 1))
+                        {
+                            if (huePanel[i + 1, j - 1] == Color.Green)
+                                neighbors++;
+                        }
+
+                        //Checks neighbor to the west
+                        if (IndicieTest(i, j - 1))
+                        {
+                            if (panelArray[i, j - 1].BackColor == Color.Green)
+                                neighbors++;
+                        }
+
+                        //checks neighbor to the northwest
+
+                        if (IndicieTest(i - 1, j - 1))
+                        {
+                            if (panelArray[i - 1, j - 1].BackColor == Color.Green)
+                                neighbors++;
+                        }
+
+                        //checks neighbor to the north
+
+                        if (IndicieTest(i - 1, j))
+                        {
+                            if (panelArray[i - 1, j].BackColor == Color.Green)
+                                neighbors++;
+                        }
+
+                        //checks neighbor to the northeast
+
+                        if (IndicieTest(i - 1, j + 1))
+                        {
+                            if (panelArray[i - 1, j + 1].BackColor == Color.Green)
+                                neighbors++;
+                        }
+
+
+
+                        //Game Logic Rules
+
+                        //Underpopulation Death
+
+                        if (neighbors < 2 && panelArray[i, j].BackColor == Color.Green)
+                        {
+                            huePanel[i, j] = Color.DarkGray;
+                            panelArray[i, j].BackColor = huePanel[i, j];
+                        }
+
+                        //lives
+
+                        else if (neighbors == 2 || neighbors == 3)
+
+                        {
+                            if (panelArray[i, j].BackColor == Color.Green)
+                            {
+                                huePanel[i, j] = Color.Green;
+                                panelArray[i, j].BackColor = huePanel[i, j];
+                            }
+                        }
+
+                        //Over Population Death
+
+                        else if (neighbors > 3 && panelArray[i, j].BackColor == Color.Green)
+
+                        {
+                            huePanel[i, j] = Color.DarkGray;
+                            panelArray[i, j].BackColor = huePanel[i, j];
+                        }
+
+                        //Reproduction
+
+                        else if (neighbors == 3 && panelArray[i,j].BackColor == Color.DarkGray)
+
+                        {
+                            huePanel[i, j] = Color.Green;
+                            panelArray[i, j].BackColor = huePanel[i, j];
+                        }
+
+                        else
+
+                        {
+                            huePanel[i, j] = Color.DarkGray;
+                        }
+
+
+
+                        Refresh();
+
+                        //panelArray[i, j].BackColor = huePanel[i, j];
+
+
+                    }
+            
+                }
+            }
+
+            
+        }
+
+        private void multiIteration_Click(object sender, EventArgs e)
+        {
+            //set var for iteration
+
+            int iteration = (int)iterationCount.Value;
+
+
+            // Loops for multiple iterations of the game
+            for (int i = 0; i < iteration; i++)
+            {
+                RunOneIteration();
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+
+    }
+}
+    
+
+
